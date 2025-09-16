f6f14981990c8dceca7ee72b731f84163227ffa4
gabney <gabney@ivytech.edu>
2025-09-15 23:24:15 -0400

    Update M03 Secure Hashing and Encryption.py
    updated sha-256 code

--- Module03/M03 Assignment Secure Hashing and Encryption/M03 Secure Hashing and Encryption.py
+++ Module03/M03 Assignment Secure Hashing and Encryption/M03 Secure Hashing and Encryption.py
@@ -29,16 +29,37 @@ import cryptography # installed library for various cryptographic functions
 
 # variables to be used later on in the program
 what_do = '' # string from user inputs to navigate to different program functions
-
+message_type = '' # string to control whether a text message or text file is what will be encrypted using SHA-256 
+txt_message = '' # string text message to generate SHA-256 hash
+txt_message_hash = '' # final hash of the txt_message input
+file_message = '' # message to be written to file for SHA-256 hash
+file_message_hash = '' # final hash of the secretmessage.txt file
 
 while True: # main program body loop
     print("Welcome to the ACME encryption program.")
-    what_do = input("Please type 'sha' for SHA-256 Encryption, 'cipher' for simple substitution encryption, or 'sign' for digital certificate signing.")
-    if what_do == "sha":
-        break
+    what_do = input("Please type 'sha' for SHA-256 Hash Generation, 'cipher' for simple substitution encryption, or 'sign' for digital certificate signing: ")
+    
+    if what_do == "sha": # SHA-256 hash generation
+        while True:
+            print("SHA-256 Hash Generation mode selected.")
+            message_type = input("Type 'text' to input a simple text message, or type 'file' to save a message to a file, or type 'q' to quit: ") #prompts user to select text or file type
+            if message_type == "text":
+                txt_message = input("Type your text message to generate a SHA-256 Hash: ").encode() # collects user text input and encodes it as bytestream, as txt_message variable
+                sha256_txt = hashlib.sha256() # initializes hashlib sha256 method with sha256_txt as the working variable
+                sha256_txt.update(txt_message) # passes txt_message bytestream to sha256_txt
+                txt_message_hash = sha256_txt.hexdigest() # converts the passed bytestream text to the final hashed format
+                print(f"Your hash value for your message is: {txt_message_hash}")
+                break
+            elif message_type == "file":
+                break
+            elif message_type == "q":
+                break
+            else:
+                print("Invalid input. Try again.")
+
     elif what_do == "cipher":
         break
     elif what_do == "sign":
         break
     else:
-        print "Invalid input. Try again."
+        print("Invalid input. Try again.")
