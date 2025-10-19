adab675b9a5642ba795912d6aaaa770e75ca2913
gabney <gabney@ivytech.edu>
2025-10-18 22:10:51 -0400

    Rename file, rename readme, add link
    Adjusted names, added test link for validations

---
+++ Module08/Module 8 Final Project - Secret Scanner/Final.py
@@ -0,0 +1,22 @@
+"""
+Module 8 Final Project - Secret Scanner
+SDEV 245 Security and Secure Coding
+Gabriel Abney
+
+This program allows the user to search for common
+regex patterns such as passwords within a specified file 
+or folder.  The interface is a CLI created with argparse.
+The results of the search are stored in a log file in the 
+same directory as the program.
+"""
+
+# Package Imports
+
+# Argparse package is used to make Command-Line-Interfaces (CLIs)
+import argparse 
+
+import re
+
+#todo 
+#argparse searchtype searchlocation searchwhat
+
---
+++ Module08/Module 8 Final Project - Secret Scanner/README - Final.txt
@@ -0,0 +1,5 @@
+Module 8 Final Project - Secret Scanner - Readme
+SDEV 245 Security and Secure Coding
+Gabriel Abney
+
+This is the readme file for the Module 08 Final Project by Gabriel Abney.
