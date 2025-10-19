6eea51bf51bafeb707ac53b7861af60717b1992c
gabney <gabney@ivytech.edu>
2025-10-19 03:50:45 -0400

    Update Final.py

--- Module08/Module 8 Final Project - Secret Scanner/Final.py
+++ Module08/Module 8 Final Project - Secret Scanner/Final.py
@@ -10,64 +10,72 @@ The results of the search are stored in a log file in the
 same directory as the program.
 """
 
+
 # Package Imports
 
 # Argparse package used to make Command-Line-Interfaces (CLIs) with better functionality
 import argparse 
-
 # Regular Expressions package used to handle regular expressions for searching text
 import re
-
 # Pathlib module for handling file and directory paths
 from pathlib import Path
 
 
+
+# ArgParse setup
+
 # Creates the ArgumentParser object for argparse
 parser = argparse.ArgumentParser(description="Argument parser for SDEV 245 Final Project") 
-
+# adds arguments to argparse object
 parser.add_argument("search_type", help="The type of search to conduct - file or directory")
 parser.add_argument("search_loc", help="The location file or directory to search")
-
-
 # Parses created arguments
 args = parser.parse_args()
 
-# regex tokens for common apps from https://github.com/odomojuli/regextokens
-twitter_token = "[1-9][0-9]+-[0-9a-zA-Z]{40}"
-google_API_key = "AIza[0-9A-Za-z-_]{35}"
-facebook_token = "EAACEdEose0cBA[0-9A-Za-z]+"
-stripe_standard_API_key = "sk_live_[0-9a-zA-Z]{24}"
-mailchimp_token = "[0-9a-f]{32}-us[0-9]{1,2}"
 
-# simplified list of above tokens for ease of use later
-tokens = [
-    twitter_token,
-    google_API_key,
-    facebook_token,
-    stripe_standard_API_key,
-    mailchimp_token
-]
 
-# log file for results to be stored
+# Regex setup
+
+# dictionary of common app tokens and their regex patterns from https://github.com/odomojuli/regextokensas
+tokens = {
+    "Twitter Token": "[1-9][0-9]+-[0-9a-zA-Z]{40}",
+    "Google API Key": "AIza[0-9A-Za-z-_]{35}",
+    "Facebook Access Token": "EAACEdEose0cBA[0-9A-Za-z]+",
+    "Stripe Standard API Key": "sk_live_[0-9a-zA-Z]{24}",
+    "Mailchimp Access Token": "[0-9a-f]{32}-us[0-9]{1,2}"
+}
+
+
+
+# Output log file for results to be stored (file will be created if it does not already exist)
 log_file = "scanresults.txt"
 
 
+
 # Functions
-def scan():
-    """Scans a file that is passed """
-    pass
+
+def scan(file_name):
+    """Scans a file that is passed for regex patterns and logs matches"""
+    with open(log_file, "a+") as log, open(file_name, "r") as f:
+        log.write(f"Scan results for {file_name}:")
+        for t in tokens:
+            pattern = re.compile(tokens[t]) # sets regex search pattern for app
+
+
+
 
 def file_search(file_name):
-    """Searches for a file in the local directory.  Must be passed a file as parameter"""
+    """Searches for a file in the local directory.  Must be passed a file name (including extension e.g. .txt) as parameter."""
     if Path.is_file(Path(file_name)): #checks to see if file exists before searching
-        scan(file_name)
-        print(f"Scanned f{file_name}. See results in {log_file}")
+        scan(file_name) # runs scan function
+        print(f"Scanned {file_name}. See results in {log_file}.") # prints confirmation to console
     else:
         print("Error. File does not exist.") # error message if file does not exist
 
 def dir_search():
     pass
     
+
 
 # Main program body
 
